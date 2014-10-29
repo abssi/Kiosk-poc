33a162244960bba8f0f935959335dd3bb1d74b89
Abssi <abssi.casanearshore.ma.ibm.com>
2014-10-29 16:31:33 +0000

    css generale update

--- Kiosk-poc/apps/Kiosk/common/js/pages/home-view.js
+++ Kiosk-poc/apps/Kiosk/common/js/pages/home-view.js
@@ -1,11 +1,39 @@
+var fr={
+		"newMortgage":"Nouveau crédit",
+		"propreties":"Mes propriétés",
+		"refinance":"Refinancement",
+		
+};
+
+var ar={
+		"newMortgage":"ائتمان جديد",
+		"propreties":"عقارات",
+		"refinance":"إعادة التمويل",
+		
+};
 
 
-
+function translate(lan){
+	$('#new-mortgage').text(lan.newMortgage);
+	$('#propreties').text(lan.propreties);
+	$('#refinance').text(lan.refinance);
+	
+}
 
 
 $("#newmortgages").click(function() {
 
 	loadPage("../pages/select-mortgage-type-view.html");
+		
+});
+$("#go-fr").click(function() {
+
+	translate(fr);
+		
+});
+$("#go-ar").click(function() {
+translate(ar);
+	
 		
 });
 
