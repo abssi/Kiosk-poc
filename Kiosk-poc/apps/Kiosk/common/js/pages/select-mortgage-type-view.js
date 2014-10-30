b4c908ce6891b7842b69f2950669d733b915fd8c
Abssi <abssi.casanearshore.ma.ibm.com>
2014-10-30 17:04:34 +0000

    update

--- Kiosk-poc/apps/Kiosk/common/js/pages/select-mortgage-type-view.js
+++ Kiosk-poc/apps/Kiosk/common/js/pages/select-mortgage-type-view.js
@@ -4,14 +4,16 @@ function initPopUp(){
 	
 	 $( "#mortgae-amount").one({
 	        popupafterclose: function() {
-	            setTimeout( function(){ 
+	        
+	            
 	            	
 	            	//alert('after close');
+	        	    
 				    selectedAmount=$('#amount').val();
 				    loadPage("../pages/mortgage-options-view.html");
 				   
 	            	
-	            }, 100 );
+	           
 	        }
 	    });
 
@@ -21,8 +23,17 @@ $("#options").one('click',function(e) {
 	initPopUp();
 	
 });
+// for validating only numerical  inputs 
+function checnum(as) {
+	var dd = as.value;
+	if (isNaN(dd)) {
+		dd = dd.substring(0, (dd.length - 1));
+		as.value = dd;
+		
+	}
+}  
 
-
+// most functions removed from here due to multiple call problem/bug
 $( document ).on( "pageinit", "#page", function(  ) {
 	
 
